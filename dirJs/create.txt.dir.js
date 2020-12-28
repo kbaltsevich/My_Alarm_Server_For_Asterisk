4413d4ee6fc20af08f56513180f8176b822ff559
Kanstantsin Baltsevich <kostielgreendevil@gmail.com>
2020-12-28 19:15:03 +0300

    feat:add function create any channels

--- dirJs/create.txt.dir.js
+++ dirJs/create.txt.dir.js
@@ -1,6 +1,6 @@
 const createFileTXT = (file_dir, file_name, header) => {
   const fs = require("fs");
-  fs.writeFile(`${file_dir}${file_name}`, `${header}`, (err) => {
+  fs.writeFile(`${file_dir}${file_name}.txt`, `${header}\r\n`, (err) => {
     if (err) console.log(err);
   });
 };
---
+++ modules/originate_channels.js
@@ -0,0 +1,32 @@
+const {createFileTXT} = require('../dirJs/create.txt.dir')
+const {write} = require('../dirJs/update_file_tab.dir')
+const {getVariableChannel} = require('../ARI/ari.variableChannels')
+const {postOriginateOne} = require('../ARI/ari.originate_one')
+
+const createChannels = async (arr_channels, count_channels) => {
+    try {
+        if(arr_channels.length & arr_channels.length > 0){
+            const file_records_name = `${Date.now()}`
+            const file_records_dir = `./text_records/`
+            const file_records_header = `My Alarm System Started ${new Date(Date.now())}`
+            createFileTXT(file_records_dir, file_records_name, file_records_header)
+            write([`${file_records_dir}${file_records_name}`, 'Абонент', 'Номер телефона', 'Начало вызова', "Конец вызова", 'Статус вызова'])
+            let count = 0;
+            while(count < arr_channels.length){
+                let count_asterisk_channel = await getVariableChannel()
+                if(count_asterisk_channel != 0 && count_asterisk_channel <= count_channels){
+                    await postOriginateOne(arr_channels[count])
+                    count++;
+                }  
+            }
+        } else {
+            console.log('Ошибка количиства канналов')
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+module.exports = {
+    createChannels
+}
